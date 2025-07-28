3658ece5686045ba2bae2dcf377b44569faafdf4
ChrisPoul <chrispoulsenfernandez@gmail.com>
2025-07-28 01:02:52 -0600

    Moved category types filter selection to layout so it's shared between home page and report page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,45 @@
 <script lang="ts">
+	import { categoryTypes } from '$lib/constants';
+	import { selectedCategoryTypes } from '$lib/stores.svelte';
+	import { getCategoryTypeLabel } from '$lib/utils';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+
+<div class="fixed top-0 left-0 z-20 flex gap-2 p-2 font-bold text-white">
+	{#each categoryTypes as category}
+		<label
+			class={`${category} flex cursor-pointer items-center gap-1.5 rounded-lg p-2 hover:scale-105`}
+		>
+			<input
+				type="checkbox"
+				class="rounded-lg"
+				bind:group={selectedCategoryTypes.value}
+				value={category}
+			/>
+			{getCategoryTypeLabel(category)}
+		</label>
+	{/each}
+	<button
+		class="flex cursor-pointer items-center gap-1.5 rounded-lg bg-slate-400 px-4 py-2 hover:scale-105"
+		onclick={() => {
+			selectedCategoryTypes.value = categoryTypes;
+		}}
+	>
+		Todo
+	</button>
+	<button
+		class="flex cursor-pointer items-center gap-1.5 rounded-lg bg-slate-400 px-4 py-2 hover:scale-105"
+		onclick={() => {
+			selectedCategoryTypes.value = [];
+		}}
+	>
+		Ninguno
+	</button>
+</div>
+
+<div class="pt-16">
+	{@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
-	import { categoryTypes } from '$lib/constants';
 	import {
 		employees,
 		categories,
 		incidenceCells,
 		isReadOnly,
-		selectedCategoryTypes,
 		selectedWeek
 	} from '$lib/stores.svelte';
-	import { getCategoryTypeLabel } from '$lib/utils';
 	import SelectCategories from '$lib/components/categories/SelectCategories.svelte';
 	import SelectEmployees from '$lib/components/employees/SelectEmployees.svelte';
 	import MainTable from '$lib/components/table/MainTable.svelte';
@@ -43,55 +40,25 @@
 	}
 </script>
 
-<div class="fixed top-0 left-0 z-20 flex gap-2 p-2 font-bold text-white">
-	<input
-		type="week"
-		class="text-black"
-		value={getWeekForInput(selectedWeek.value?.startDate)}
-		onchange={async (e: Event) => {
-			const week = (e.target as HTMLInputElement).value;
-			const res = await fetch('/api/weeks', {
-				method: 'POST',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify({ week })
-			});
-			const { weekId } = await res.json();
-			window.location.href = `/?weekId=${weekId}`;
-		}}
-	/>
-	{#each categoryTypes as category}
-		<label
-			class={`${category} flex cursor-pointer items-center gap-1.5 rounded-lg p-2 hover:scale-105`}
-		>
-			<input
-				type="checkbox"
-				class="rounded-lg"
-				bind:group={selectedCategoryTypes.value}
-				value={category}
-			/>
-			{getCategoryTypeLabel(category)}
-		</label>
-	{/each}
-	<button
-		class="flex cursor-pointer items-center gap-1.5 rounded-lg bg-slate-400 px-4 py-2 hover:scale-105"
-		onclick={() => {
-			selectedCategoryTypes.value = categoryTypes;
-		}}
-	>
-		Todo
-	</button>
-	<button
-		class="flex cursor-pointer items-center gap-1.5 rounded-lg bg-slate-400 px-4 py-2 hover:scale-105"
-		onclick={() => {
-			selectedCategoryTypes.value = [];
-		}}
-	>
-		Ninguno
-	</button>
-</div>
-
-<div class="pt-16">
-	<h1 class="text-center text-2xl font-bold">{getWeekTitle(selectedWeek.value)}</h1>
+<div>
+	<div class="flex items-center gap-3 pl-4">
+		<input
+			type="week"
+			class=" text-black"
+			value={getWeekForInput(selectedWeek.value?.startDate)}
+			onchange={async (e: Event) => {
+				const week = (e.target as HTMLInputElement).value;
+				const res = await fetch('/api/weeks', {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({ week })
+				});
+				const { weekId } = await res.json();
+				window.location.href = `/?weekId=${weekId}`;
+			}}
+		/>
+		<h1 class="text-center text-2xl font-bold">{getWeekTitle(selectedWeek.value)}</h1>
+	</div>
 	<MainTable />
 </div>
 
